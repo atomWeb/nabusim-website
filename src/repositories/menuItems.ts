47b3eeff07654cfa5601a65d895d6c1aba31dd9a
nabusim <cristianjose@gmail.com>
2023-08-23 20:55:34 +0200

    my astro website first commit

---
+++ src/repositories/menuItems.ts
@@ -0,0 +1,20 @@
+import type { MenuItem } from '../types/types';
+
+const menuItems: MenuItem[] = [
+  {
+    title: 'Sobre mi',
+    url: '/about',
+    icon: '',
+  },
+  {
+    title: 'Proyectos',
+    url: '/work',
+    icon: '',
+  },
+  {
+    title: 'Contacta',
+    url: '/contact',
+    icon: '',
+  },
+];
+export default menuItems;
